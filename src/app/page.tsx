288bae37a14f537296878f8a093c70e1211a163c
EnkiDeniz <denizsengun@hotmail.com>
2026-03-28 21:18:26 -0400

    Create Home component for main page

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', fontFamily: 'system-ui, sans-serif' }}>
+      <h1>Assembled Reality</h1>
+    </main>
+  )
+}
